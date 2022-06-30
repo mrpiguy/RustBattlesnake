aa7fe881d8211211ae55e23338fc76b2d7cf08cf
mrpiguy <96925175+mrpiguy@users.noreply.github.com>
2022-06-30 15:47:59 -0400

    Initial Commit
    Minor changes to the starter project

---
+++ src/main.rs
@@ -0,0 +1,130 @@
+#[macro_use]
+extern crate rocket;
+
+use log::info;
+use rocket::fairing::AdHoc;
+use rocket::http::Status;
+use rocket::serde::{json::Json, Deserialize};
+use serde::Serialize;
+use serde_json::{json, Value};
+use std::collections::HashMap;
+use std::env;
+mod logic;
+
+//Structures
+
+//GameState struct
+#[derive(Deserialize, Serialize, Debug)]
+pub struct GameState {
+    game: Game,
+    turn: u32,
+    board: Board,
+    you: Battlesnake,
+}
+
+//Coordinate used for storing coordinates within other structs
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Coord {
+    x: u32,
+    y: u32,
+}
+//Game object used to store all data about a game
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Game {
+    id: String,
+    ruleset: HashMap<String, Value>,
+    timeout: u32,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Board {
+    height: u32,
+    width: u32,
+    food: Vec<Coord>,
+    snakes: Vec<Battlesnake>,
+    hazards: Vec<Coord>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Battlesnake {
+    id: String,
+    name: String,
+    health: u32,
+    body: Vec<Coord>,
+    head: Coord,
+    length: u32,
+    latency: String,
+
+    // Used in non-standard game modes
+    shout: Option<String>,
+    squad: Option<String>,
+}
+
+
+//API IMPLEMENTATION
+#[get("/")]
+fn handle_index() -> Json<Value> {
+    Json(logic::get_info())
+}
+
+#[post("/start", format = "json", data = "<start_req>")]
+fn handle_start(start_req: Json<GameState>) -> Status {
+    logic::start(
+        &start_req.game,
+        &start_req.turn,
+        &start_req.board,
+        &start_req.you,
+    );
+
+    Status::Ok
+}
+
+#[post("/move", format = "json", data = "<move_req>")]
+fn handle_move(move_req: Json<GameState>) -> Json<Value> {
+    let chosen = logic::get_move(
+        &move_req.game,
+        &move_req.turn,
+        &move_req.board,
+        &move_req.you,
+    );
+
+    Json(json!({ "move": chosen }))
+}
+
+#[post("/end", format = "json", data = "<end_req>")]
+fn handle_end(end_req: Json<GameState>) -> Status {
+    logic::end(&end_req.game, &end_req.turn, &end_req.board, &end_req.you);
+
+    Status::Ok
+}
+
+#[launch]
+fn rocket() -> _ {
+    // Lots of web hosting services expect you to bind to the port specified by the `PORT`
+    // environment variable. However, Rocket looks at the `ROCKET_PORT` environment variable.
+    // If we find a value for `PORT`, we set `ROCKET_PORT` to that value.
+    if let Ok(port) = env::var("PORT") {
+        env::set_var("ROCKET_PORT", &port);
+    }
+
+    // We default to 'info' level logging. But if the `RUST_LOG` environment variable is set,
+    // we keep that value instead.
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "info");
+    }
+
+    env_logger::init();
+
+    info!("Starting Battlesnake Server...");
+
+    rocket::build()
+        .attach(AdHoc::on_response("Server ID Middleware", |_, res| {
+            Box::pin(async move {
+                res.set_raw_header("Server", "BattlesnakeOfficial/starter-snake-rust");
+            })
+        }))
+        .mount(
+            "/",
+            routes![handle_index, handle_start, handle_move, handle_end],
+        )
+}
